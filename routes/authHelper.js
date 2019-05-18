1a3a62bf5382eb4d66d7fad83a7f953dfce24d6e
Tyquan <tyquanjreddick@gmail.com>
2019-05-18 00:41:28 -0400

    added authHelper

---
+++ routes/authHelper.js
@@ -0,0 +1,18 @@
+const jwt = require('jwt-simple');
+
+module.expoorts.checkAuth = (req, res, next) => {
+    if (req.headers['x-auth']) {
+        try {
+            req.auth = jwt.decode(req.headers['x-auth'], process.env.JWT_SECRET);
+            if (req.auth && req.auth.authorized && req.auth.userId) {
+                return next();
+            } else {
+                return next(new Error('User is not logged in.'));
+            }
+        } catch (err) {
+            return next(err);
+        }
+    } else {
+        return next(new Error('User is not logged in.'));
+    }
+};
