de8fbc6de0a9b302a93dd7b3abd48653fad459dd
Tyquan <tyquanjreddick@gmail.com>
2019-05-21 00:05:48 -0400

    added app reducer

---
+++ src/reducers/app.js
@@ -0,0 +1,26 @@
+const initialState = {
+    loggedIn: false,
+    session: null,
+    currentMsg: ""
+}
+
+const appLevel = (state=initialState, action) => {
+    switch(action.type) {
+        case 'MSG_DISPLAY':
+            return {
+                ...state,
+                currentMsg: action.msg
+            }
+        case 'RECEIVE_TOKEN_SUCCESS':
+            return {
+                ...state,
+                loggedIn: true,
+                session: action.session,
+                currentMsg: action.msg
+            }
+        default:
+            return state;
+    }
+};
+
+export default appLevel;
---
+++ src/reducers/index.js
@@ -0,0 +1,12 @@
+import {combineReducers} from 'redux';
+import app from './app';
+// import homeposts from './homeposts'
+// import posts from './posts'
+// import sharedposts from './sharedposts'
+// import profile from './profile'
+
+const rootReducer = combineReducers({
+    app
+})
+
+export default rootReducer;
